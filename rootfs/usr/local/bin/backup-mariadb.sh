7908bfb6423e688cf362fa5d80a3ebeb41c156d7
Gabe Cook <git@gabe565.com>
2026-03-04 02:28:18 -0600

    feat: add `backup-mariadb.sh`

---
+++ rootfs/usr/local/bin/backup-mariadb.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -u
+
+: "${DB_PASSWORD_FILE:=/mariadb/mariadb-password}"
+
+if [ -f "$DB_PASSWORD_FILE" ]; then
+  read -r DB_PASSWORD < "$DB_PASSWORD_FILE"
+fi
+
+export MYSQL_PWD="$DB_PASSWORD"
+
+set -ex
+mariadb-dump --add-drop-table --host="$DB_HOST" --user="$DB_USER" "$DB_NAME"
