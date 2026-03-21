d1c78a5ba80e02365b09633a6c4128ca9028d202
Gabe Cook <git@gabe565.com>
2026-03-20 19:12:23 -0500

    perf: update CNPG backup script to use a static restrict key

--- rootfs/usr/local/bin/backup-cnpg.sh
+++ rootfs/usr/local/bin/backup-cnpg.sh
@@ -9,7 +9,11 @@ read -r DB_USER < "$CNPG_SECRET_PATH/username"
 read -r PGPASSWORD < "$CNPG_SECRET_PATH/password"
 export PGPASSWORD
 
+if [[ -z "${PG_RESTRICT_KEY:-}" ]]; then
+  PG_RESTRICT_KEY="$(sha256sum <<<"$DB_HOST$DB_NAME$DB_USER$PGPASSWORD" | cut -d' ' -f1)"
+fi
+
 set -x
-exec pg_dump --clean --if-exists --no-owner \
+exec pg_dump --clean --if-exists --no-owner --restrict-key="$PG_RESTRICT_KEY" \
   --host="$DB_HOST" --username="$DB_USER" --dbname="$DB_NAME" \
   "$@"
